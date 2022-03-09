418e11668db7c6d0fc7456863344b82ca9449aef
Chris petre <christophepetre@telecomnancy.eu>
2022-03-09 15:29:16 +0100

    ajout des composants pour la nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,28 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Route,  BrowserRouter as Router, Routes } from 'react-router-dom';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import FolderPage from './pages/FolderPage';
 import ClientPage from './pages/ClientPage';
 import NotFoundPage from './pages/NotFoundPage';
+import Layout from './layout/Layout';
 
 function App() {
   return (
     <Router>
-      <div> 
-        <Routes>
-          <Route path='/' element={<HomePage />} />
-          <Route path='/accueil' element={<HomePage />} />
-          <Route path='/clients' element={<ClientPage />} />
-          <Route path='/dossiers' element={<FolderPage />} /> 
-          <Route path='*' element={<NotFoundPage  />} />
-        </Routes >
+      <div>
+        <Layout>
+          <Routes>
+            <Route path='/' element={<HomePage />} />
+            <Route path='/accueil' element={<HomePage />} />
+            <Route path='/clients' element={<ClientPage />} />
+            <Route path='/dossiers' element={<FolderPage />} />
+            <Route path='/dossiers/en-cours' element={<FolderPage />} />
+            <Route path='/dossiers/cloturees' element={<FolderPage />} />
+            <Route path='*' element={<NotFoundPage />} />
+          </Routes >
+        </Layout>
       </div>
     </Router>
   );
