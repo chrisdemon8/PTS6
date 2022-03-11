fbfeebea267d010ba0209a320a5c95ae2a6ab255
Chris petre <christophepetre@telecomnancy.eu>
2022-03-11 12:24:07 +0100

    ajout build electron

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import './App.css'; 
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'; 
 import HomePage from './pages/HomePage';
 import FolderPage from './pages/FolderPage';
 import ClientPage from './pages/ClientPage';
