f08047c29a1ee1478053a9c04e9b7a399fc77f8c
chrisdemon8 <christophepetre57@gmail.com>
2022-04-07 16:10:21 +0200

    last update front

--- src/pages/FolderPage.tsx
+++ src/pages/FolderPage.tsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react';
 import ModalComponent from '../components/modal/Modal';
 import AdapterDateFns from '@mui/lab/AdapterDateFns';
 import { Button, MenuItem, TextField } from '@mui/material';
+import styles from './css/clientdetail.module.css';
 import { Table } from '../components/table/Table';
 import Select, { SelectChangeEvent } from '@mui/material/Select';
 import { getCases, saveCase } from '../components/request/callapiCase';
@@ -45,18 +46,22 @@ const FolderPage = () => {
 
   return (
     <>
-      <h1>Listes des affaires</h1>
-      <Button onClick={handleDialogOpen}>Ajouter une affaire</Button>
-      {<Table columns={[
-        { title: "Code", field: "case_id", filtering: false },
-        { title: "Statut", field: "statusLabel" },
-        { title: "Clients", field: "concernedClientLabel", filtering: false },
-      ]}
-        dataFrom={dataCases}
-        nameId="case_id"
-      />}
+      <div className={styles.content}>
+        <div className={styles.contenttablepage}>
+          <h1>Listes des affaires</h1>
+          <Button onClick={handleDialogOpen}>Ajouter une affaire</Button>
 
+        </div>
+        {<Table columns={[
+          { title: "Code", field: "case_id", filtering: false },
+          { title: "Statut", field: "statusLabel" },
+          { title: "Clients", field: "concernedClientLabel", filtering: false },
+        ]}
+          dataFrom={dataCases}
+          nameId="case_id"
+        />}
 
+      </div>
       <ModalComponent isOpen={isOpen} handleClose={handleDialogClose}
         buttonAction={
           <Button onClick={handleSubmit} variant="contained" color="primary">
